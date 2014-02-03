dcc2a8f617b912054d3a5a2b685668d7e85f9abc
Yan Wei <asurada.gen@gmail.com>
2014-02-03 16:49:15 +0900

    add

---
+++ proj.ios/class/BaseScene.cpp
@@ -0,0 +1,9 @@
+//
+//  BaseScene.cpp
+//  sangokuCutX
+//
+//  Created by Yan Wei on 2014/02/03.
+//
+//
+
+#include "BaseScene.h"
---
+++ proj.ios/class/BaseScene.h
@@ -0,0 +1,14 @@
+//
+//  BaseScene.h
+//  sangokuCutX
+//
+//  Created by Yan Wei on 2014/02/03.
+//
+//
+
+#ifndef __sangokuCutX__BaseScene__
+#define __sangokuCutX__BaseScene__
+
+#include <iostream>
+
+#endif /* defined(__sangokuCutX__BaseScene__) */
